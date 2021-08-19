89a753df3c93a7d052e20af70c23a2dbecc8c541
StunxFS <56417208+StunxFS@users.noreply.github.com>
2021-08-19 09:58:53 -0400

    format `script.js`

--- src/script.js
+++ src/script.js
@@ -1,18 +1,15 @@
 let switcher = document.getElementById("switch")
-let contnt = document.getElementById("content")
+let content = document.getElementById("content")
 let icon = document.getElementById("iconchange")
 let word = document.getElementById("spanchange")
 
 switcher.onclick = function() {
-    if(!contnt.classList.contains("white"))
-    {
-        contnt.classList.add("white")
+    if (!content.classList.contains("white")) {
+        content.classList.add("white")
         icon.className = "bx bxs-moon"
         word.textContent = "Oscuridad"
-    }
-    else
-    {
-        contnt.classList.remove("white")
+    } else {
+        content.classList.remove("white")
         icon.className = "bx bxs-sun"
         word.textContent = "Luz"
     }
